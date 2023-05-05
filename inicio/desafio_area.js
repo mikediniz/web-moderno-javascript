c3db3eec588a43fcefcd4700f503a6d45c63ef34
mikediniz <mikediniz.system@gmail.com>
2023-05-05 04:29:16 -0300

    Iniciando com Programação

---
+++ inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI = 3.141592...
+// PI * raio * raio
+
+const PI = 3.141592;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("Ovalor da área é " + areaCirc + "m2.");
+
+areaCirc = Math.PI * raio * raio;
+console.log("Ovalor da área é " + areaCirc + "m2.");
---
+++ inicio/sentenca_1.js
@@ -0,0 +1,4 @@
+console
+    .log("Bom dia!");
+console.log("Boa Tarde!");
+console.log("Boa Noite!");
